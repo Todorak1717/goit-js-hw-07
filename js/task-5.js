560aa703925e89890eb942ab5e99f040c2c0dc87
Todorak1717 <155604203+Todorak1717@users.noreply.github.com>
2024-05-23 22:48:30 -0500

    up

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.body;
+const changesColor = document.querySelector('.change-color');
+const currentColor = document.querySelector('.color');
+
+changesColor.addEventListener('click', ()=>{
+const newColor = getRandomHexColor();
+body.style.backgroundColor =
+newColor;
+currentColor.textContent = newColor;
+});
