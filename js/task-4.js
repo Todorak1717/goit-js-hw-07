079755a53dbf68c438f2946188f39a864e709247
Todorak1717 <155604203+Todorak1717@users.noreply.github.com>
2024-05-23 22:08:59 -0500

    up

--- js/task-4.js
+++ js/task-4.js
@@ -1,13 +1,15 @@
 const form = document.querySelector('.login-form');
 form.addEventListener('submit', e => {
     e.preventDefault();
-    const emailName = form.elements.email.value.trim();
-    const passwordName = form.elements.password.value.trim();
+   const email = form.elements.email.value.trim();
+      const password = form.elements.password.value.trim();
+    
+    const data ={ email , password};
   
-if(emailName === '' || passwordName === ''){
+if(email === '' || password === ''){
     return alert('All form fields must be filled in');
    }
-   const data = {emailName , passwordName};
+ 
 
 console.log(data);
 form.reset()
